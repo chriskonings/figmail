6864da5725b15bb75a6588730692aa097e4b19d8
Chris Konings <chriskonings@CK.local>
2018-10-22 12:46:52 -0700

    code formatting

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 const PERSONAL_ACCESS_TOKEN = '4378-ec89dc8d-6301-4ee7-ae8f-8fa7341a0f43';
 
 function getFileKey(pageUrl) {
-const parser = document.createElement('a');
-parser.href = pageUrl;
-return parser.pathname.replace('/file/', '').replace(/\/.*/,'');
+  const parser = document.createElement('a');
+  parser.href = pageUrl;
+  return parser.pathname.replace('/file/', '').replace(/\/.*/,'');
 }
 
 function getNodeId(pageUrl) {
-const parser = document.createElement('a');
-parser.href = pageUrl;
-return decodeURIComponent(parser.search).replace('?node-id=','');
+  const parser = document.createElement('a');
+  parser.href = pageUrl;
+  return decodeURIComponent(parser.search).replace('?node-id=','');
 }
 
 async function apiRequest(fileKey, id) {
   return fetch('/convert?key=' + fileKey + '&id=' + id)
-    .then(function(response) {
-      return response.json();
-    }).catch(function (error) {
-      return { err: error };
-    });
+  .then(function(response) {
+    return response.json();
+  }).catch(function (error) {
+    return { err: error };
+  });
 }
 
 async function callFigmaAndDrawMockups() {
